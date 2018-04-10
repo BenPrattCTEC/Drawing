73ebcac29553668babd0b28fa94ecdf4eaf25309
bcpratt2000@gmail.com <bcpratt2000@gmail.com>
2018-04-10 13:49:26 -0600

    populated DrawingPanel

--- src/drawing/view/DrawingPanel.java
+++ src/drawing/view/DrawingPanel.java
@@ -6,15 +6,37 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Calendar;
+import java.awt.geom.*;
+import java.util.Hashtable;
 
 import javax.swing.*;
-import javax.swing.text.DefaultCaret;
+import javax.swing.event.*;
 
 public class DrawingPanel extends JPanel {
 	
+	private final int MINIMUM_EDGE = 5;
+	private final int MAXIMUM_EDGE = 20;
+	private final int MINIMUM_SCALE = 20;
+	private final int MAXIMUM_SCALE = 100;
+	
 	private Controller controller;
 	private SpringLayout layout;
+	
+	private ShapeCanvas canvas;
+	private JPanel buttonPanel;
+	private JPanel sliderPanel;
+	private JSlider scaleSlider;
+	private JSlider edgeSlider;
+	private JButton triangleButton;
+	private JButton rectangleButton;
+	private JButton ellipseButton;
+	private JButton polygonButton;
+	private JButton clearButton;
+	private JButton saveButton;
+	private JButton colorButton;
+	
+	private int currentEdgeCount;
+	private int currentScale;
 	
 	public DrawingPanel(Controller controller) {
 		super();
@@ -22,9 +44,26 @@ public class DrawingPanel extends JPanel {
 		
 		layout = new SpringLayout();
 		
+		currentScale = MINIMUM_SCALE;
+		currentEdgeCount = MINIMUM_EDGE;
+		scaleSlider = new JSlider(MINIMUM_SCALE, MAXIMUM_SCALE);
+		edgeSlider = new JSlider(MINIMUM_EDGE, MAXIMUM_EDGE);
+		
+		canvas = new ShapeCanvas(controller);
+		sliderPanel = new JPanel();
+		buttonPanel = new JPanel(new GridLayout(0, 1));
+		
+		triangleButton = new JButton("Add Triangle");
+		rectangleButton = new JButton("Add Rectangle");
+		ellipseButton = new JButton("Add Ellipse");
+		polygonButton = new JButton("Add Polygon");
+		clearButton = new JButton("Clear");
+		saveButton = new JButton("Save");
+		colorButton = new JButton("Chanhe Color");
 		
 		
-		setupScrollPane();
+		
+		setupSliders();
 		setupPanel();
 		setupLayout();
 		setupListeners();
@@ -33,7 +72,33 @@ public class DrawingPanel extends JPanel {
 	/*
 	 * Sets up the scroll pane
 	 */
-	private void setupScrollPane(){
+	private void setupSliders(){
+		Hashtable<Integer, JLabel> scaleLabels = new Hashtable<Integer, JLabel>();
+		Hashtable<Integer, JLabel> edgeLabels = new Hashtable<Integer, JLabel>();
+		
+		scaleLabels.put(MINIMUM_SCALE, new JLabel("<HTML>Small<BR>Shape</HTML>"));
+		scaleLabels.put((MAXIMUM_SCALE+MINIMUM_SCALE)/2, new JLabel("<HTML>Medium<BR>Shape</HTML>"));
+		scaleLabels.put(MAXIMUM_SCALE, new JLabel("<HTML>Large<BR>Shape</HTML>"));
+
+		edgeLabels.put(MINIMUM_EDGE, new JLabel("Edges: " + MINIMUM_EDGE));
+		edgeLabels.put(MAXIMUM_EDGE, new JLabel("Edges: " + MAXIMUM_EDGE));
+		
+		scaleSlider.setLabelTable(scaleLabels);
+		scaleSlider.setOrientation(JSlider.VERTICAL);
+		scaleSlider.setSnapToTicks(true);
+		scaleSlider.setMajorTickSpacing(10);
+		scaleSlider.setPaintTicks(true);
+		scaleSlider.setPaintLabels(true);
+
+		edgeSlider.setLabelTable(edgeLabels);
+		edgeSlider.setOrientation(JSlider.VERTICAL);
+		edgeSlider.setSnapToTicks(true);
+		edgeSlider.setMajorTickSpacing(3);
+		edgeSlider.setMinorTickSpacing(1);
+		edgeSlider.setPaintTicks(true);
+		edgeSlider.setPaintLabels(true);
+		
+		
 	}
 	
 	/**
@@ -42,6 +107,27 @@ public class DrawingPanel extends JPanel {
 	private void setupPanel() {
 		this.setBackground(Color.darkGray);
 		this.setLayout(layout);
+		
+		this.setPreferredSize(new Dimension(200, 450));
+		this.add(canvas);
+		buttonPanel.add(triangleButton);
+		buttonPanel.add(rectangleButton);
+		buttonPanel.add(ellipseButton);
+		buttonPanel.add(polygonButton);
+		buttonPanel.add(clearButton);
+		buttonPanel.add(saveButton);
+		buttonPanel.add(colorButton);
+		
+		sliderPanel.setPreferredSize(new Dimension(250, 450));
+		sliderPanel.add(scaleSlider);
+		sliderPanel.add(edgeSlider);
+		
+		this.add(buttonPanel);
+		this.add(sliderPanel);
+	}
+	
+	private boolean coinFlip(){
+		return Math.random() < .5;
 	}
 	
 	/**
